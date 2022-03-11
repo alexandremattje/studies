70115df5433f2b59450f2bf40dde43d355acc494
Alexandre Mattje <alexandre.mattje@lessen.com>
2022-03-11 15:45:21 -0300

    initial commit

---
+++ back/java/src/test/java/io/mattje/MattjeIOTest.java
@@ -0,0 +1,13 @@
+package io.mattje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MattjeIOTest {
+
+    @Test
+    void contextLoads() {
+    }
+    
+}
